1770efa18c99d3017366ab834aae25d6caa115a5
Edinyanga O. Ottoho <elzucky@gmail.com>
2021-07-22 10:52:49 +0100

    Create mailerliteapi.sql

---
+++ mailerliteapi.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jul 22, 2021 at 09:47 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mailerliteapi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `server`
+--
+
+CREATE TABLE `server` (
+  `id` int(255) NOT NULL,
+  `api_key` text NOT NULL,
+  `date_created` int(255) NOT NULL,
+  `date_updated` int(255) NOT NULL,
+  `subscriber_group` varchar(255) NOT NULL DEFAULT 'NULL'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `server`
+--
+
+INSERT INTO `server` (`id`, `api_key`, `date_created`, `date_updated`, `subscriber_group`) VALUES
+(1, '0', 0, 0, 'NULL');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `server`
+--
+ALTER TABLE `server`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `server`
+--
+ALTER TABLE `server`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
